1fd4395d72f099e339e6c6c41a4040ff5c707921
bryaneduarr <bryaneduarr@gmail.com>
2024-07-09 21:03:11 -0600

    Clase 07-09-2024

--- app/src/main/java/com/example/http_request_crud/ActivityCreate.java
+++ app/src/main/java/com/example/http_request_crud/ActivityCreate.java
@@ -1,15 +1,28 @@
 package com.example.http_request_crud;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,7 +31,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.http_request_crud.Config.RestApiMethods;
 import com.example.http_request_crud.Models.Personas;
@@ -26,12 +39,25 @@ import com.example.http_request_crud.Models.Personas;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ActivityCreate extends AppCompatActivity {
   private RequestQueue requestQueue;
+
   Button buttonFoto, buttonGuardar;
 
+  ImageView imageView;
+
   EditText nombres, apellidos, direccion, telefono;
+
+  static final int REQUEST_IMAGE = 101;
+
+  static final int ACCESS_CAMERA = 201;
+
+  String currentPhotoPath;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +69,99 @@ public class ActivityCreate extends AppCompatActivity {
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
       return insets;
     });
+    imageView = (ImageView) findViewById(R.id.imageView);
     nombres = (EditText) findViewById(R.id.nombreEditText);
     apellidos = (EditText) findViewById(R.id.apellidoEditText);
     telefono = (EditText) findViewById(R.id.telefonoEditText);
     direccion = (EditText) findViewById(R.id.direccionEditText);
+    // Buttons
+    buttonFoto = (Button) findViewById(R.id.imgaeViewButton);
+    buttonGuardar = (Button) findViewById(R.id.buttonGuardar);
+
+    buttonFoto.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        ObtenerFoto();
+      }
+    });
+
+    buttonGuardar.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+  }
+
+  private void ObtenerFoto() {
+    // Metodo para obtener los permisos requeridos de la aplicacion
+    if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, ACCESS_CAMERA);
+    } else {
+      dispatchTakePictureIntent();
+
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (requestCode == ACCESS_CAMERA) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        dispatchTakePictureIntent();
+      } else {
+        Toast.makeText(getApplicationContext(), "se necesita el permiso de la camara", Toast.LENGTH_LONG).show();
+      }
+    }
+  }
+
+  private File createImageFile() throws IOException {
+    // Create an image file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String imageFileName = "JPEG_" + timeStamp + "_";
+    File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    File image = File.createTempFile(imageFileName,  /* prefix */
+            ".jpg",         /* suffix */
+            storageDir      /* directory */);
+
+    // Save a file: path for use with ACTION_VIEW intents
+    currentPhotoPath = image.getAbsolutePath();
+    return image;
+  }
+
+  private void dispatchTakePictureIntent() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    // Ensure that there's a camera activity to handle the intent
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+      // Create the File where the photo should go
+      File photoFile = null;
+      try {
+        photoFile = createImageFile();
+      } catch (IOException ex) {
+        ex.toString();
+      }
+      // Continue only if the File was successfully created
+      if (photoFile != null) {
+        Uri photoURI = FileProvider.getUriForFile(this, "com.example.http_request_crud.fileprovider", photoFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE);
+      }
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == REQUEST_IMAGE) {
+      try {
+        File foto = new File(currentPhotoPath);
+        imageView.setImageURI(Uri.fromFile(foto));
+      } catch (Exception ex) {
+        ex.toString();
+      }
+    }
   }
 
   private void SendDataCreate() {
@@ -57,6 +172,7 @@ public class ActivityCreate extends AppCompatActivity {
     person.setNombres(apellidos.getText().toString());
     person.setNombres(telefono.getText().toString());
     person.setNombres(direccion.getText().toString());
+    person.setFoto(ConvertImageBase64(currentPhotoPath));
 
     JSONObject jsonObject = new JSONObject();
 
@@ -70,9 +186,9 @@ public class ActivityCreate extends AppCompatActivity {
       Log.d("Error Response", error.toString());
     }
 
-    StringRequest stringRequest = new StringRequest(Request.Method.POST, RestApiMethods.EndPointGET, new Response.Listener<String>() {
+    JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, RestApiMethods.EndPointGET, jsonObject, new Response.Listener<JSONObject>() {
       @Override
-      public void onResponse(String response) {
+      public void onResponse(JSONObject response) {
         try {
           String message = response.toString();
         } catch (Exception error) {
@@ -115,6 +231,5 @@ public class ActivityCreate extends AppCompatActivity {
     bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
     byte[] imagearray = byteArrayOutputStream.toByteArray();
     return Base64.encodeToString(imagearray, Base64.DEFAULT);
-
   }
 }
